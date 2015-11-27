c9d15fcad6c159e362f4ce3b27d53c6b37680aa6
lucas <lucaspsimpson@gmail.com>
2015-11-27 14:11:17 +0000

    Added python script which works on image for coins

--- src/watershed.cpp
+++ src/watershed.cpp
@@ -1,4 +1,8 @@
 #include <opencv2/opencv.hpp>
+//#include <opencv2/core/utility.hpp>
+//#include <opencv2/imgproc.hpp>
+//#include <opencv2/imgcodecs.hpp>
+//#include <opencv2/highgui.hpp>
 #include <iostream>
 using namespace std;
 using namespace cv;
@@ -13,19 +17,19 @@ int main(int, char** argv)
         return -1;
     // Show source image
     imshow("Source Image", src);
-    // Change the background from white to black, since that will help later to extract
-    // better results during the use of Distance Transform
-    for( int x = 0; x < src.rows; x++ ) {
-      for( int y = 0; y < src.cols; y++ ) {
-          if ( src.at<Vec3b>(x, y) == Vec3b(255,255,255) ) {
-            src.at<Vec3b>(x, y)[0] = 0;
-            src.at<Vec3b>(x, y)[1] = 0;
-            src.at<Vec3b>(x, y)[2] = 0;
-          }
-        }
-    }
-    // Show output image
-    imshow("Black Background Image", src);
+//    // Change the background from white to black, since that will help later to extract
+//    // better results during the use of Distance Transform
+//    for( int x = 0; x < src.rows; x++ ) {
+//      for( int y = 0; y < src.cols; y++ ) {
+//          if ( src.at<Vec3b>(x, y) == Vec3b(255,255,255) ) {
+//            src.at<Vec3b>(x, y)[0] = 0;
+//            src.at<Vec3b>(x, y)[1] = 0;
+//            src.at<Vec3b>(x, y)[2] = 0;
+//          }
+//        }
+//    }
+//    // Show output image
+//    imshow("Black Background Image", src);
     // Create a kernel that we will use for accuting/sharpening our image
     Mat kernel = (Mat_<float>(3,3) <<
             1,  1, 1,
@@ -46,6 +50,8 @@ int main(int, char** argv)
     imgResult.convertTo(imgResult, CV_8UC3);
     imgLaplacian.convertTo(imgLaplacian, CV_8UC3);
     // imshow( "Laplace Filtered Image", imgLaplacian );
+
+
     imshow( "New Sharped Image", imgResult );
     src = imgResult; // copy back
     // Create binary image from source image
@@ -53,67 +59,12 @@ int main(int, char** argv)
     cvtColor(src, bw, CV_BGR2GRAY);
     threshold(bw, bw, 40, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
     imshow("Binary Image", bw);
-    // Perform the distance transform algorithm
-    Mat dist;
-    distanceTransform(bw, dist, CV_DIST_L2, 3);
-    // Normalize the distance image for range = {0.0, 1.0}
-    // so we can visualize and threshold it
-    normalize(dist, dist, 0, 1., NORM_MINMAX);
-    imshow("Distance Transform Image", dist);
-    // Threshold to obtain the peaks
-    // This will be the markers for the foreground objects
-    threshold(dist, dist, .4, 1., CV_THRESH_BINARY);
-    // Dilate a bit the dist image
-    Mat kernel1 = Mat::ones(3, 3, CV_8UC1);
-    dilate(dist, dist, kernel1);
-    imshow("Peaks", dist);
-    // Create the CV_8U version of the distance image
-    // It is needed for findContours()
-    Mat dist_8u;
-    dist.convertTo(dist_8u, CV_8U);
-    // Find total markers
-    vector<vector<Point> > contours;
-    findContours(dist_8u, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-    // Create the marker image for the watershed algorithm
-    Mat markers = Mat::zeros(dist.size(), CV_32SC1);
-    // Draw the foreground markers
-    for (size_t i = 0; i < contours.size(); i++)
-        drawContours(markers, contours, static_cast<int>(i), Scalar::all(static_cast<int>(i)+1), -1);
-    // Draw the background marker
-    circle(markers, Point(5,5), 3, CV_RGB(255,255,255), -1);
-    imshow("Markers", markers*10000);
-    // Perform the watershed algorithm
-    watershed(src, markers);
-    Mat mark = Mat::zeros(markers.size(), CV_8UC1);
-    markers.convertTo(mark, CV_8UC1);
-    bitwise_not(mark, mark);
-//    imshow("Markers_v2", mark); // uncomment this if you want to see how the mark
-                                  // image looks like at that point
-    // Generate random colors
-    vector<Vec3b> colors;
-    for (size_t i = 0; i < contours.size(); i++)
-    {
-        int b = theRNG().uniform(0, 255);
-        int g = theRNG().uniform(0, 255);
-        int r = theRNG().uniform(0, 255);
-        colors.push_back(Vec3b((uchar)b, (uchar)g, (uchar)r));
-    }
-    // Create the result image
-    Mat dst = Mat::zeros(markers.size(), CV_8UC3);
-    // Fill labeled objects with random colors
-    for (int i = 0; i < markers.rows; i++)
-    {
-        for (int j = 0; j < markers.cols; j++)
-        {
-            int index = markers.at<int>(i,j);
-            if (index > 0 && index <= static_cast<int>(contours.size()))
-                dst.at<Vec3b>(i,j) = colors[index-1];
-            else
-                dst.at<Vec3b>(i,j) = Vec3b(0,0,0);
-        }
-    }
-    // Visualize the final image
-    imshow("Final Result", dst);
+
+    //find_connected components
+
+ //   int nLabels = connectedComponents(bw, labelImage, 8);
+
+
     waitKey(0);
     return 0;
 }
